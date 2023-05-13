1770324d071cec0493faef230a583a9f04bc35d7
Ammar <ammarferchichi7@gmail.com>
2023-05-13 01:08:43 +0100

    the 2 components and bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weatherApp';
+  city:any = '';
+
+  onInput(target:any){
+    this.city = target.value;
+  }
 }
