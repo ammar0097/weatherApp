589690a69d532a411e7892cf1cba25be9e7131f8
Ammar <ammarferchichi7@gmail.com>
2023-05-14 20:58:01 +0100

    Display data

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -5,27 +5,22 @@ import { WeatherService } from '../shared/weather.service';
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css'],
 })
-export class CurrentWeatherComponent    {
-  
-  constructor(private weatherService : WeatherService){}
-  weatherData:any;
-  @Input() city: string = '';
-
-  onInput(target:any){
+export class CurrentWeatherComponent {
+  constructor(private weatherService: WeatherService) {}
+  weatherData: any;
+  city:any;
+  onInput(target: any) {
     this.city = target.value;
   }
-
-  getWeather(): void{
-    this.weatherService.getWeather(this.city).subscribe((data) => {
-      this.weatherData = data;
-      console.log(data);
-      
-    },
-    (error) =>{
-      console.log(error)
-    }
-    )
-
+  getWeather(): void {
+    this.weatherService.getWeather(this.city).subscribe({
+      next: (data) => {
+        this.weatherData = data;
+        console.log(data);
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
   }
-  
 }
--- src/app/shared/weather.service.ts
+++ src/app/shared/weather.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class WeatherService {
-  apiKey = 'd01c359cbe6c09a550d3bf235e96fe9f';
+ private apiKey = 'd01c359cbe6c09a550d3bf235e96fe9f';
   constructor(private http: HttpClient) {}
   getWeather(city: string): Observable<any> {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`;
